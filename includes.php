de2b15b1ef0081b359f91d9b751a7b65eb7a053a
bluepichu <bluepichu64@gmail.com>
2015-04-20 14:41:26 -0400

    Main page structure

---
+++ includes.php
@@ -0,0 +1,24 @@
+<?php function make_head($title){ ?>
+<head>
+	<title><?php print($title); ?></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<?php } ?>
+
+<?php function make_header(){ ?>
+<header>
+	<!-- Header content goes here. -->
+</header>
+<?php } ?>
+
+<?php function make_sidebar(){ ?>
+<section id="sidebar">
+	<!-- Sidebar content goes here. -->
+</section>
+<?php } ?>
+
+<?php function make_footer(){ ?>
+<footer>
+	<!-- Footer content goes here. -->
+</footer>
+<?php } ?>
