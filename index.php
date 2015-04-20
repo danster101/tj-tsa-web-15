78bf1f41b1d40c978f4663a94088b548e777825a
bluepichu <bluepichu64@gmail.com>
2015-04-20 15:03:02 -0400

    Change file extensions

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<?php include("includes.php"); ?>
+	<?php make_head("Home"); ?>
+	<body>
+		<?php make_header(); ?>
+		<?php make_sidebar(); ?>
+		<main>
+			
+		</main>
+		<?php make_footer(); ?>
+	</body>
+</html>
