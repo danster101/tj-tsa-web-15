319e5fd2f04d518b3daf96d17aa418f3061ff008
bluepichu <bluepichu64@gmail.com>
2015-04-21 19:41:23 -0400

    Various

--- includes.php
+++ includes.php
@@ -2,8 +2,9 @@
 <head>
 	<title><?php print($title); ?></title>
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link href='//fonts.googleapis.com/css?family=Martel+Sans:800' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Magra:400,700' rel='stylesheet' type='text/css'>
+	<link href="//fonts.googleapis.com/css?family=Martel+Sans:800" rel="stylesheet" type="text/css">
+	<link href="//fonts.googleapis.com/css?family=Magra:400,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <?php } ?>
 
@@ -16,7 +17,14 @@
 
 <?php function make_sidebar(){ ?>
 <section id="sidebar">
-	<!-- Sidebar content goes here. -->
+	<nav>
+		<ul>
+			<li><span class="fa nav-icon fa-home"></span>Home</li>
+			<li><span class="fa nav-icon fa-users"></span>Our Chapter</li>
+			<li><img class="nav-icon" src="images/tsa-simplified-black.svg"></span>Technology Student Association</li>
+			<li><span class="fa nav-icon fa-book"></span>Projects</li>
+		</ul>
+	</nav>
 </section>
 <?php } ?>
 
--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 		<?php make_sidebar(); ?>
 		<main>
 			<article>
-				<h2>Lorem ipsum</h2>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+				<h2>Next Stop: Technosphere!</h2>
+				<p>Hello everyone!  <b>Technosphere</b> (the Virginia TSA State Conference) is coming up soon!  Make sure to get your fees to your officers as soon as possible, and don't forget to prepare for all of your events!</p>
 			</article>
 
 			<article>
