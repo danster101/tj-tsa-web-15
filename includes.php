93071f9cd2f8011eb161b45fd97da6a33477833b
bluepichu <bluepichu64@gmail.com>
2015-04-21 21:48:21 -0400

    Shrinking header

--- includes.php
+++ includes.php
@@ -1,10 +1,27 @@
 <?php function make_head($title){ ?>
 <head>
 	<title><?php print($title); ?></title>
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link href="//fonts.googleapis.com/css?family=Martel+Sans:800" rel="stylesheet" type="text/css">
-	<link href="//fonts.googleapis.com/css?family=Magra:400,700" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/reset.css" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+	<link href="//fonts.googleapis.com/css?family=Martel+Sans:800" rel="stylesheet" type="text/css" />
+	<link href="//fonts.googleapis.com/css?family=Magra:400,700" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+	<script>
+		$(document).ready(function(){
+			$(window).scroll(function(e){
+				if($(window).scrollTop() > $("header h1").height()){
+					$("body").addClass("scroll-small-header");
+				} else {
+					$("body").removeClass("scroll-small-header");
+				}
+			});
+		});
+	</script>
 </head>
 <?php } ?>
 
--- index.php
+++ index.php
@@ -14,6 +14,16 @@
 				<h2>Dolor sit amet</h2>
 				<p>Bacon ipsum dolor amet swine pariatur chuck short loin meatloaf boudin proident labore tongue ham. Pork belly pastrami deserunt, short loin fugiat consequat landjaeger minim drumstick shoulder excepteur tempor bresaola nisi tenderloin. Jerky duis pig incididunt aliquip dolore. Tempor sausage ut incididunt in quis dolore, biltong aute short ribs chuck pork belly minim.</p>
 			</article>
+			
+			<article>
+				<h2>Dolor sit amet</h2>
+				<p>Bacon ipsum dolor amet swine pariatur chuck short loin meatloaf boudin proident labore tongue ham. Pork belly pastrami deserunt, short loin fugiat consequat landjaeger minim drumstick shoulder excepteur tempor bresaola nisi tenderloin. Jerky duis pig incididunt aliquip dolore. Tempor sausage ut incididunt in quis dolore, biltong aute short ribs chuck pork belly minim.</p>
+			</article>
+			
+			<article>
+				<h2>Dolor sit amet</h2>
+				<p>Bacon ipsum dolor amet swine pariatur chuck short loin meatloaf boudin proident labore tongue ham. Pork belly pastrami deserunt, short loin fugiat consequat landjaeger minim drumstick shoulder excepteur tempor bresaola nisi tenderloin. Jerky duis pig incididunt aliquip dolore. Tempor sausage ut incididunt in quis dolore, biltong aute short ribs chuck pork belly minim.</p>
+			</article>
 		</main>
 		<?php make_footer(); ?>
 	</body>
