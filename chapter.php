416451ab8f610858063aeb5358bac3e50053c289
bluepichu <bluepichu64@gmail.com>
2015-04-21 21:58:35 -0400

    Add missing files

---
+++ chapter.php
@@ -0,0 +1,14 @@
+<html>
+	<?php include("includes.php"); ?>
+	<?php make_head("Our Chapter"); ?>
+	<body>
+		<?php make_header(); ?>
+		<?php make_sidebar(); ?>
+		<main>
+			<h2>Our Chapter</h2>
+			
+			<p>Some content goes here.  Probably sub-pages.</p>
+		</main>
+		<?php make_footer(); ?>
+	</body>
+</html>
