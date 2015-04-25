4b1951d92fa86a3dfd7a477dfa38fedb814ce63b
bluepichu <bluepichu64@gmail.com>
2015-04-25 12:35:28 -0400

    All sorts of things

--- chapter/index.php
+++ chapter/index.php
@@ -5,13 +5,26 @@
 		<?php make_header(); ?>
 		<?php make_sidebar(); ?>
 		<main>
-			<h2>The Chapter</h2>
+			<article>
+				<h2>The Chapter</h2>
 
-			<div class="member-info-widget">
-				<img src="../images/team-small.jpg" class="blur"/>
-				<img src="../images/team-small.jpg" class="sharp"/>
-				<div class="name-label">Name Here</div>
-			</div>
+				<nav class="page-subnav">
+					<ul>
+						<li load="about" class="active"><span class="fa nav-icon fa-info"></span>About</li>
+						<li load="awards"><span class="fa nav-icon fa-trophy"></span>Awards</li>
+					</ul>
+				</nav>
+				
+				<div class="content"></div>
+			</article>
+
+			<!--<div class="member-info-widget">
+<img src="../images/team-small.jpg" class="blur"/>
+<img src="../images/team-small.jpg" class="sharp"/>
+<div class="name-label">Name Here</div>
+</div>-->
+
+
 		</main>
 		<?php make_footer(); ?>
 		<script>
@@ -29,10 +42,10 @@
 					name: "Kyle Herndon"
 				}
 			];
-			
+
 			var width = 720;
 			var height = 540;
-			
+
 			$(document).ready(function(){
 				$(".member-info-widget").mousemove(function(e){
 					var x = e.pageX - $(this).offset().left;
@@ -40,14 +53,14 @@
 					for(var i = 0; i < data.length; i++){
 						if(Math.pow(x - data[i].cx, 2) + Math.pow(y - data[i].cy, 2) <= Math.pow(data[i].radius, 2)){
 							var rule = "circle(" + data[i].radius  + "px at " + data[i].cx + "px " + data[i].cy + "px)";
-							
+
 							$(".member-info-widget .sharp").css({
 								"clip-path": rule,
 								"-webkit-clip-path": rule
 							});
-							
+
 							$(".member-info-widget .name-label").text(data[i].name);
-							
+
 							return;
 						}
 					}
