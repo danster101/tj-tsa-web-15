3484919785bbdf4478179cfa672caa7285977272
Danial Hussain <dhussain101@gmail.com>
2015-04-25 18:36:10 -0400

    Chapter About V 0.5

--- chapter/about.php
+++ chapter/about.php
@@ -1,1 +1,30 @@
-<h3>About</h3>
+<h3>History</h3>
+
+<h4>Pre-2012</h4>
+
+<p>TJ had some attempts to begin TSA chapters in the past. However, most of them only lasted a single year before disbanding.</p>
+
+<h4>2012-2013</h4>
+
+<p>The current TJHSST TSA Chapter was founded in 2012 by Lucas Lin and Kyle Herndon. Danial Hussain and Matthew Savage aided in establishing the chapter and making sure members were ready for the regional, state, and national conferences. The chapter was mostly filled with members who were graduates from the Rachel Carson Middle School TSA Chapter. That year, the chapter had 25 members.</p>
+
+<p>The chapter's first tests of its ability came in 2013 with the Northern Regional Fair and Technosphere. TJ TSA took home 13 each at the Regional Fair and at Technosphere. TJ's Rohan Pandit won <i>Most Competitive Technosphere Participant</i>, and the school as a whole won the <i>Arvid W. Van Dyke Outstanding School Award</i>.</p>
+
+<p>Seven of TJ TSA's members made the trip to Orlando for the National Conference in June 2013. The team had a handful of finalist finishes and a single trophy, won by Matthew Barbano for Future Technology Teacher.</p>
+
+<div class="float-left" style="width: 225px">
+    <img src="images/florida-nationals-2013.jpg" width=225 />
+    <span class="subtitle">The logo of the 2013 National Conference held in Orlando, Florida.</span>
+</div>
+
+<h4>2013-2014</h4>
+
+<p>When the TSA season began again in fall 2013, the chapter had gained much popularity and had established itself as a highly competitive club. Unlike the previous year, a strict applications process was used to determine members, and about 30% of applicants were rejected, producing a competing team of about 34 students. The chapter also had a different sponsor and meeting location than the previous year.</p>
+
+<p>TJ TSA delivered once again at the 2014 Northern Regional Fair, bringing home 18 trophies and 13 additional finalist finishes totalling 31 top 10 event finishes, notably including a first-second-third sweep in Debating Technological Issues.</p>
+
+<p>Outdoing their regional performance and gaining experience competing at the state level, TJ TSA went to Technosphere 2014 and won 19 trophies and 18 additional finalist awards to end up with 37 top 10 results. For the second consecutive year, the chapter won the <i>Arvid W. Van Dyke Outstanding School Award</i>.</p>
+
+<h3>Contact Us</h3>
+
+<p>The TSA officers can be contacted at <a href="mailto:tsa.at.tjhsst@gmail.com">tsa.at.tjhsst@gmail.com</a> and Mr. Piccione can be reached at <a href="mailto:mspiccione@fcps.edu">mspiccione@fcps.edu</a>.</p>
