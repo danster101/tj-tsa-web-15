a259d727dc37cb76afe88e824500c5ffa61c6ba9
Danial Hussain <dhussain101@gmail.com>
2015-04-26 08:58:18 -0400

    Chapter About V 0.7

--- chapter/about.php
+++ chapter/about.php
@@ -8,14 +8,14 @@
 
 <p>The current TJHSST TSA Chapter was founded in 2012 by Lucas Lin and Kyle Herndon. Danial Hussain and Matthew Savage aided in establishing the chapter and making sure members were ready for the regional, state, and national conferences. The chapter was mostly filled with members who were graduates from the Rachel Carson Middle School TSA Chapter. That year, the chapter had 25 members.</p>
 
+<figure float="right" style="width: 200px">
+    <img src="/tsa/images/florida-nationals-2013.jpg" width="200" />
+    <figcaption>The logo of the 2013 National Conference held in Orlando, Florida.</figcaption>
+</figure>
+
 <p>The chapter's first tests of its ability came in 2013 with the Northern Regional Fair and Technosphere. TJ TSA took home 13 each at the Regional Fair and at Technosphere. TJ's Rohan Pandit won <i>Most Competitive Technosphere Participant</i>, and the school as a whole won the <i>Arvid W. Van Dyke Outstanding School Award</i>.</p>
 
 <p>Seven of TJ TSA's members made the trip to Orlando for the National Conference in June 2013. The team had a handful of finalist finishes and a single trophy, won by Matthew Barbano for Future Technology Teacher.</p>
-
-<figure float="left" style="width: 225px">
-    <img src="/tsa/images/florida-nationals-2013.jpg" width="225px" />
-    <figcaption>The logo of the 2013 National Conference held in Orlando, Florida.</figcaption>
-</figure>
 
 <h4>2013-2014</h4>
 
@@ -25,6 +25,10 @@
 
 <p>Outdoing their regional performance and gaining experience competing at the state level, TJ TSA went to Technosphere 2014 and won 19 trophies and 18 additional finalist awards to end up with 37 top 10 results. For the second consecutive year, the chapter won the <i>Arvid W. Van Dyke Outstanding School Award</i>.</p>
 
+<p>In between Technosphere and the state tournament, TJ TSA participated in J-Day or Jefferson Day. This is TJHSST's annual field-day like event where school clubs have booths and are allowed to sell food and other items to raise funds. In addition, the Student Government sets up several moon-bounces and waterslides and there are several game rooms. TJ TSA raised over $200 selling fried oreos, donuts, twinkies, and other sweet treats.</p>
+
+<p>This year TJ TSA had a much greater presence at the 2014 National TSA Conference because it was in our backyard in Washington D.C. We improved greatly from last year, taking home 4 trophies (2 first place) and 4 additional finalist awards for a total of 8 top 10 finishes.</p>
+
 <h3>Contact Us</h3>
 
-<p>The TSA officers can be contacted at <a href="mailto:tsa.at.tjhsst@gmail.com">tsa.at.tjhsst@gmail.com</a> and Mr. Piccione can be reached at <a href="mailto:mspiccione@fcps.edu">mspiccione@fcps.edu</a>.</p>
+<p>The TSA officers can be contacted at <a href="mailto:tsa.at.tjhsst@gmail.com">tsa.at.tjhsst@gmail.com</a> and our sponsor, Mr. Piccione, can be reached at <a href="mailto:mspiccione@fcps.edu">mspiccione@fcps.edu</a>.</p>
