17f5d822359cb6fdf75c0f70c706bd31876e5fc2
bluepichu <bluepichu64@gmail.com>
2015-04-27 22:58:37 -0400

    That's what people say

--- widgets/slider.php
+++ widgets/slider.php
@@ -1,5 +1,27 @@
+<script src="/tsa/js/jquery.sequence-min.js"></script>
+<link rel="stylesheet" type="text/css" href="/tsa/css/slider.css" />
+<script>
+$(document).ready(function(){
+	$("#sequence").sequence().data("sequence");
+});
+</script>
 <div class="slider-wrapper">
-	<div class="slider">
-		<img src="//placehold.it/1600x900" width="100%" />
+	<div class="slider-container">
+		<div class="slider" id="sequence">
+			<ul class="sequence-canvas">
+				<li id="welcome">
+					<img id="tsa-logo" src="/tsa/images/tsa-color.svg" />
+					<div id="at">@</div>
+					<img id="tj-logo" src="/tsa/images/tj-logo.jpg" />
+					<div id="welcome-text">Welcome to TSA at TJHSST</div>
+				</li>
+				<li>
+					
+				</li>
+				<li>
+					
+				</li>
+			</ul>
+		</div>
 	</div>
 </div>
