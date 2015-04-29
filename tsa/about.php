848e9f07910257e2759004ec26bbf93cf4b4d1ea
Danial Hussain <dhussain101@gmail.com>
2015-04-29 16:18:38 -0400

    And the dirty dirty cheats of the world

---
+++ tsa/about.php
@@ -0,0 +1,23 @@
+<p><b>TSA</b>, or <b>Technology Student Association</b>, is a technology-themed student group that provides students with opportunities in STEM via a variety of activities including co-curricular activities, service projects, and competitive events.</p>
+
+<h4>Mission</h4>
+
+<p>On <a target="_blank" href="http://tsaweb.org">its website</a>, TSA provides the following mission for what it hopes to accomplish:</p>
+
+<p class="indented"><i>"The Technology Student Association fosters personal growth, leadership, and opportunities in technology, innovation, design, and engineering. Members apply and integrate science, technology, engineering and mathematics concepts through co-curricular activities, competitive events and related programs."</i></p>
+
+<h4>Motto</h4>
+
+<p class="indented"><i>"Learning to Live in a Technical World"</i></p>
+
+<h4>Creed</h4>
+
+<p>TSA also provides a creed by which its students strive to live and work:</p>
+
+<i>
+    <p>I believe that Technology Education holds an important place in my life in the technical world. I believe there is a need for the development of good attitudes concerning work, tools, materials, experimentation, and processes of industry.</p>
+    <p>Guided by my teachers, artisans from industry, and my own initiative, I will strive to do my best in making my school, community, state, and nation better places in which to live.</p>
+    <p>I will accept the responsibilities that are mine. I will accept the theories that are supported by proper evidence. I will explore on my own for safer, more effective methods of working and living.</p>
+    <p>I will strive to develop a cooperative attitude and will exercise tact and respect for other individuals. Through the work of my hands and mind, I will express my ideas to the best of my ability.</p>
+    <p>I will make it my goal to do better each day the task before me, and to be steadfast in my belief in my God, and my fellow Americans."</p>
+</i>
