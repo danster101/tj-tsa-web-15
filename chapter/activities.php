fcae595f0c301ffbe4ce6e069403800deea23024
Danial Hussain <dhussain101@gmail.com>
2015-04-29 15:23:33 -0400

    Hey Hey Hey

---
+++ chapter/activities.php
@@ -0,0 +1,5 @@
+<h3>Chapter Activities</h3>
+
+<h4>J-Day</h4>
+
+<p>Every June Thomas Jefferson HS has a one day celebration known as Jefferson Day or J-Day! There are very short classes, giving Student Government Association time to set up and </p>
