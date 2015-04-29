fb5361054b74ad1a89f43e13b46411e6672140c0
Danial Hussain <dhussain101@gmail.com>
2015-04-29 16:38:22 -0400

    She's like oh my god

--- chapter/awards.php
+++ chapter/awards.php
@@ -1,1 +1,402 @@
-<h3>Awards</h3>
+<p><b>Bolded</b> names and teams placed in their respective competitions and advanced to the next level of competition, if applicable.</p>
+
+<p><i>Italicized</i> names and teams were finalists in their respective competitions.</p>
+
+<h4>2014 Technosphere</h4>
+
+<figure>
+    <img src="/tsa/images/techno2014a.jpeg" />
+</figure>
+
+<div>
+    <div>
+        <h5>Animatronics</h5>
+        <p><i>Adam Kim, Shrikant Mishra, Peter Zhao, Nathan Kim, and Nayana Suvarna</i></p>
+    </div>
+    <div>
+        <h5>Biotechnology Design</h5>
+        <p><i>Amee Kapadia, Raksha Pothapragada, Sruthi Jayaraman, Jadan Doodala, and Pranay Singh</i></p>
+    </div>
+    <div>
+        <h5>Career Preparation</h5>
+        <p><b>Asha Krishnakumar</b> - 2nd</p>
+    </div>
+    <div>
+        <h5>Chapter Team</h5>
+        <p><i>Pranay Singh, Lucas Lin, George Perry, Matthew Savage, Danial Hussain, and Kyle Herndon</i></p>
+    </div>
+    <div>
+        <h5>Debating Technological Issues</h5>
+        <p><i>Raksha Pothapragada and Sruthi Jayaraman</i></p>
+        <p><i>Amee Kapadia and Pranavi Nara</i></p>
+    </div>
+    <div>
+        <h5>Desktop Publishing</h5>
+        <p><b>Pranay Singh</b> - 1st</p>
+        <p><b>Raksha Pothapragada</b> - 4th</p>
+    </div>
+    <div>
+        <h5>Engineering Design</h5>
+        <p><i>Pranay Singh, Adam Kim, Caelan Barranta, Ellis Tsung, and Peter Zhao</i></p>
+    </div>
+    <div>
+        <h5>Essays on Technology</h5>
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Asha Krishnakumar</b> - 2nd</p>
+    </div>
+    <div>
+        <h5>Extemporaneous Presentation</h5>
+        <p><i>Asha Krishnakumar</i></p>
+    </div>
+    <div>
+        <h5>Future Technology Teacher</h5>
+        <p><b>Pranay Singh</b> - 3rd</p>
+        <p><i>Amee Kapadia</i></p>
+        <p><i>Shrikant Mishra</i></p>
+    </div>
+    <div>
+        <h5>Geospatial Technology</h5>
+        <p><i>Jagan Doodala, Caelan Barranta, Nayana Suvarna, and Isaac Iyengar</i></p>
+        <p><i>Raksha Pothapragada, Sruthi Jayaraman, and George Perry</i></p>
+        <p><i>Amee Kapadia and Pranavi Nara</i></p>
+    </div>
+    <div>
+        <h5>On Demand Video</h5>
+        <p><b>Jagan Doodala, Justin Redman, George Perry, Shrikant Mishra, and Nathan Kim</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Open Source Software Development</h5>
+        <p><b>Zach Wade, Matthew Savage, Danial Hussain, Siva Somayyajula, and Kyle Herndon</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Photographic Technology</h5>
+        <p><b>Justin Redman</b> - 2nd</p>
+    </div>
+    <div>
+        <h5>Prepared Presentation</h5>
+        <p><b>Lucas Lin</b> - 3rd</p>
+        <p><i>Pranavi Nara</i></p>
+    </div>
+    <div>
+        <h5>Principles of Technology</h5>
+        <p><b>Lucas Lin, Matthew Savage, Danial Hussain, and Kyle Herndon</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Promotional Graphics</h5>
+        <p><b>Ellis Tsung</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>SciVis</h5>
+        <p><b>John Feng, Ben Zhang, Zach Wade, Peter Zhao, and Sashank Thupukari</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Technical Sketching and Application</h5>
+        <p><b>Kyle Herndon</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Technology Bowl (Oral)</h5>
+        <p><b>Lucas Lin, Matthew Savage, and Kyle Herndon</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Technology Bowl (Written)</h5>
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Matthew Savage</b> - 2nd</p>
+        <p><i>Kyle Herndon</i></p>
+    </div>
+    <div>
+        <h5>Video Game Design</h5>
+        <p><i>Ellis Tsung, Ben Zhang, and Zach Wade</i></p>
+    </div>
+    <div>
+        <h5>Webmaster</h5>
+        <p><b>Ben Zhang, Matthew Savage, Danial Hussain, Siva Somayyajula, and Sashank Thupukari</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Member of the Year</h5>
+        <p><b>Kyle Herndon</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>William T. Reed Most Competitive Technosphere Participant Award</h5>
+        <p><b>Kyle Herndon</b> - 2nd</p>
+        <p><b>Matthew Savage</b> - 6th</p>
+        <p><b>Lucas Lin</b> - 7th</p>
+    </div>
+    <div>
+        <h5>Arvid W. Van Dyke Outstanding School Award</h5>
+        <p><b>Thomas Jefferson High School for Science and Technology</b> - 1st</p>
+    </div>
+</div>
+
+<p><a href="http://virginiatsa.org/assets/2012/06/TechnolosphereWinners20141.html" target="_blank">Full 2014 Technosphere Results</a></p>
+
+<h4>2014 Northern Regional Fair</h4>
+
+<img src="/tsa/images/regional-awards.jpg" width="100%" />
+
+<div>
+    <div>
+        <h5>Career Preparation</h5>
+        <p><b>Asha Krishnakumar</b> - 2nd</p>
+        <p><i>Pranay Singh</i></p>
+        <p><i>Pranavi Nara</i></p>
+    </div>
+    <div>
+        <h5>Debating Technological Issues</h5>
+        <p><b>Amee Kapida and Pranavi Nara</b> - 1st</p>
+        <p><b>Raksha Pothaprogada and Sruthi Jayaraman</b> - 2nd</p>
+        <p><b>Jagan Doodala and Rohan Pandit</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Desktop Publishing</h5>
+        <p><b>Pranay Singh</b> - 2nd</p>
+        <p><b>Raksha Pothapragada</b> - 3rd</p>
+        <p><i>George Perry</i></p>
+    </div>
+    <div>
+        <h5>Essays on Technology</h5>
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Asha Krishnakumar</b> - 2nd</p>
+        <p><i>Sruthi Jayaraman</i></p>
+    </div>
+    <div>
+        <h5>Extemporaneous Presentation</h5>
+        <p><b>Asha Krishnakumar</b> - 3rd</p>
+        <p><i>Kyle Herndon</i></p>
+        <p><i>Zach Wade</i></p>
+    </div>
+    <div>
+        <h5>Flight Endurance</h5>
+        <p><b>Rohan Pandit</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Manufacturing Prototype</h5>
+        <p><b>Adam Kim, John Feng, and Quinn Dawkins</b> - 1st</p>
+        <p><i>Justin Redman, Peter Zhao, Isaac Iyengar, and George Perry</i></p>
+    </div>
+    <div>
+        <h5>Music Production</h5>
+        <p><b>Adam Kim, Caelan Barranta, and Prerana Katiyar</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Photographic Technology</h5>
+        <p><b>Justin Redman</b> - 2nd</p>
+        <p><b>George Perry</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Prepared Presentation</h5>
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Pranavi Nara</b> - 2nd</p>
+        <p><i>Amee Kapida</i></p>
+        <p><i>Danial Hussain</i></p>
+    </div>
+    <div>
+        <h5>Promotional Graphics</h5>
+        <p><b>Ellis Tsung</b> - 1st <a href="projects/#feprographic" class="project-link">Project</a></p>
+        <p><i>Peter Zhao</i></p>
+    </div>
+    <div>
+        <h5>System Control Technology</h5>
+        <p><i>Danial Hussain, Matthew Savage, and Siva Somayyajula</i></p>
+    </div>
+    <div>
+        <h5>Video Game Design</h5>
+        <p><b>Ben Zhang, Ellis Tsung, and Zach Wade</b> - 2nd <a href="projects/#robox" class="project-link">Project</a></p>
+        <p><i>Danial Hussain, Kyle Herndon, Lucas Lin, and Matthew Savage</i></p>
+        <p><i>Nathan Kim, Patrick Do, Peter Zhao, Shrikant Mishra, and Siva Somayyajula</i></p>
+    </div>
+</div>
+
+<p><a href="http://virginiatsa.org/assets/2012/06/Northern2014a.html" target="_blank">Full 2014 Regional Fair Results</a></p>
+
+<h4>2013 National Conference</h4>
+
+<div>
+    <div>
+        <h5>Future Technology Teacher</h5>
+        <p><b>Matthew Barbano</b> - 2nd</p>
+        <p><i>Pranay Singh</i></p>
+    </div>
+    <div>
+        <h5>Technical Sketching and Application</h5>
+        <p><i>Matthew Savage</i></p>
+    </div>
+    <div>
+        <h5>Technology Bowl, Written</h5>
+        <p><i>Matthew Savage</i></p>
+    </div>
+    <div>
+        <h5>Technology Bowl, Oral</h5>
+        <p><i>Matthew Savage, Rohan Pandit, and Matthew Barbano</i></p>
+    </div>
+</div>
+
+<p><a href="https://www.thinkregistration.com/tsa1213/results.aspx" target="_blank">Full 2013 National Conference Results</a></p>
+
+<h4>2013 Technosphere</h4>
+
+<img src="/tsa/images/techno2013.jpg" width="100%" />
+
+<div>
+    <div>
+        <h5>Biotechnology Design</h5>
+        <p><b>Sruthi Jayuaraman, Raksha Pothapragada, Rohan Pandit, Pranay Singh, and Dhruv Gupta</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Career Preparation</h5>
+        <p><b>Asha Krishnakumar</b> - 1st</p>
+        <p><i>Sruthi Jayaraman</i></p>
+    </div>
+    <div>
+        <h5>Debating Technological Issues</h5>
+        <p><b>Lucas Lin and Rohan Pandit</b> - 1st</p>
+        <p><i>Sruthi Jayaraman and Raksha Pothapragada</i></p>
+    </div>
+    <div>
+        <h5>Desktop Publishing</h5>
+        <p><b>Raksha Pothapragada</b> - 1st</p>
+        <p><i>Pranay Singh</i></p>
+    </div>
+    <div>
+        <h5>Essays on Technology</h5>
+        <p><b>Lucas Lin</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Extemporaneous Presentation</h5>
+        <p><i>Lucas Lin</i></p>
+        <p><i>Kyle Herndon</i></p>
+    </div>
+    <div>
+        <h5>Flight Endurance</h5>
+        <p><i>Rohan Pandit</i></p>
+    </div>
+    <div>
+        <h5>Future Technology Teacher</h5>
+        <p><i>George Perry</i></p>
+        <p><i>Pranay Singh</i></p>
+    </div>
+    <div>
+        <h5>Geospatial Technology</h5>
+        <p><b>Sruthi Jayaraman, Raksha Pothapragada, and George Perry</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Music Production</h5>
+        <p><i>George Perry and Justin Redman</i></p>
+    </div>
+    <div>
+        <h5>On Demand Video</h5>
+        <p><i>George Perry, Ellis Tsung, and Dhruv Gupta</i></p>
+    </div>
+    <div>
+        <h5>Open Source Software Development</h5>
+        <p><b>Matthew Savage and Danial Hussain</b> - 3rd <a href="projects/#dimcalc" class="project-link">Project</a></p>
+    </div>
+    <div>
+        <h5>Principles of Technology</h5>
+        <p><b>Rohan Pandit, Kyle Herndon, Danial Hussain, and Matthew Savage</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Technology Bowl, Written</h5>
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Rohan Pandit</b> - 3rd</p>
+        <p><i>Matthew Savage</i></p>
+    </div>
+    <div>
+        <h5>Technology Bowl, Oral</h5>
+        <p><b>Lucas Lin, Rohan Pandit, and Matthew Savage</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Webmaster</h5>
+        <p><b>Matthew Savage, Danial Hussain, and Zach Wade</b> - 2nd <a href="projects/#web2013" class="project-link">Project</a></p>
+    </div>
+    <div>
+        <h5>William T. Reed Most Competitive Technosphere Participant Award</h5>
+        <p><b>Rohan Pandit</b> - 1st</p>
+        <p><b>Lucas Lin</b> - 5th</p>
+    </div>
+    <div>
+        <h5>Arvind W. Van Dyke Outstanding School Award</h5>
+        <p><b>Thomas Jefferson High School for Science and Technology</b> - 1st</p>
+    </div>
+</div>
+
+<p><a href="http://virginiatsa.org/assets/2012/06/TSA-Awards4.html" target="_blank">Full 2013 Technosphere Results</a></p>
+
+<h4>2013 Northern Regional Fair</h4>
+
+<div>
+    <div>
+        <h5>Career Preparation</h5>
+        <p><b>Asha Krishnakumar</b> - 1st</p>
+        <p><b>Sruthi Jayaraman</b> - 3rd</p>
+        <p><i>Pranay Singh</i></p>
+    </div>
+    <div>
+        <h5>Debating Technological Issues</h5>
+        <p><b>Sruthi Jayaraman and Raksha Pothapragada</b> - 2nd</p>
+        <p><b>Lucas Lin and Rohan Pandit</b> - 3rd</p>
+        <p><i>Danial Hussain and Jeffery Liu</i></p>
+    </div>
+    <div>
+        <h5>Desktop Publishing</h5>
+        <p><b>Pranay Singh</b> - 1st</p>
+        <p><b>Raksha Pothapragada</b> - 3rd</p>
+    </div>
+    <div>
+        <h5>Digital Video Production</h5>
+        <p><i>Pranay Singh, Sashank Thupukari, and Arvind Chava</i></p>
+    </div>
+    <div>
+        <h5>Essays on Technology</h5>
+        <p><b>Lucas Lin</b> - 2nd</p>
+        <p><b>Asha Krishnakumar</b> - 3rd</p>
+        <p><i>Sashank Thupukari</i></p>
+    </div>
+    <div>
+        <h5>Extemporaneous Presentation</h5>        
+        <p><b>Lucas Lin</b> - 1st</p>
+        <p><b>Kyle Herndon</b> - 2nd</p>
+        <p><i>Justin Redman</i></p>
+    </div>
+    <div>
+        <h5>Flight Endurance</h5>
+        <p><b>Rohan Pandit</b> - 1st</p>
+    </div>
+    <div>
+        <h5>Manufacturing Prototype</h5>
+        <p><i>Harsha Jonnavithula, Venkata Doodala, and Henry Ellis</i></p>
+    </div>
+    <div>
+        <h5>Music Production</h5>
+        <p><b>Justin Redman and George Perry</b> - 3rd</p>
+        <p><i>Zach Wade, Kyle Herndon, and Dhruv Gupta</i></p>
+    </div>
+    <div>
+        <h5>Photographic Technology</h5>
+        <p><i>Jeffery Liu</i></p>
+    </div>
+    <div>
+        <h5>Prepared Presentation</h5>
+        <p><b>Asha Krishnakumar</b> - 1st</p>
+        <p><i>Raksha Pothapragada</i></p>
+    </div>
+    <div>
+        <h5>Promotional Graphics</h5>
+        <p><i>Matthew Savage</i></p>
+        <p><i>Sashank Thupukari</i></p>
+    </div>
+    <div>
+        <h5>System Control Technology</h5>
+        <p><i>Jack Fang, Siva Somayyajula, and Harshal Jariwala</i></p>
+    </div>
+    <div>
+        <h5>Transportation Modeling</h5>
+        <p><i>Harsha Jonnavithula</i></p>
+    </div>
+    <div>
+        <h5>Video Game Design</h5>
+        <p><i>Matthew Barbano, Zach Wade, Kyle Herndon, and Ellis Tsung</i></p>
+        <p><i>Aditya Ponukumati, Siva Somayyajula, and Rishub Jain</i></p>
+    </div>
+</div>
+
+<p><a href="http://virginiatsa.org/assets/2012/06/NO_2013.html" target="_blank">Full 2013 Regional Results</a></p>
