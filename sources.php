8b824868f4c78919b9ea6221bfabe64626a203b8
bluepichu <github@bluepichu.com>
2015-04-30 15:22:07 -0400

    Baby I'm just gonna shake, shake, shake, shake, shake

--- sources.php
+++ sources.php
@@ -3,12 +3,14 @@
 	<?php make_head("Sources"); ?>
 	<body>
 		<?php make_header("Sources"); ?>
-		<?php make_sidebar(); ?>
-		<main>
-			<article>
-				<p>Sources go here.</p>
-			</article>
-		</main>
+		<div class="flex" id="content-row">
+			<?php make_sidebar(); ?>
+			<main>
+				<article>
+					<p>Sources go here.</p>
+				</article>
+			</main>
+		</div>
 		<?php make_footer(); ?>
 	</body>
 </html>
