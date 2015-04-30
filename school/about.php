13b6c858039576b92b4ccfea6f7958b2f7e7ec23
Danial Hussain <dhussain101@gmail.com>
2015-04-30 15:09:26 -0400

    A

--- school/about.php
+++ school/about.php
@@ -1,3 +1,7 @@
+<figure float="left" style="width: 200px">
+    <img src="/tsa/images/school/about/tj-logo.png" width="200px" />
+</figure>
+
 <p>Thomas Jefferson High School for Science and Technology was first founded as Thomas Jefferson High School in Alexandria, VA in 1964 and was then officially made a selective VA Public Governer's school in 1985. <a href="http://tjhsst.edu" target="_blank">TJHSST</a> is part of Fairfax County Public Schools (<a href="http://www.fcps.edu" target="_blank">FCPS</a>). The school is public, but a special admissions process was created that involves a standardized test, teacher recommendations, and past achievements. The TJHSST admissions process is tweaked each year to adapt to the changing educational environment. The current principle is Dr. Evan Glazer, who earned a B.S in Mathematics, a M.S. in Mathematics Education, and a Ph.D. in Instructoral Technology (Alumni of University of Illinois and University of Georgia). A full staff directory can be found <a href="http://www.tjhsst.edu/staff_directory.html" target="_blank">here</a>.</p> 
 
 <h4>Awards and Honors</h4>
@@ -8,7 +12,7 @@
 
     <li>From 2002 - 2010 TJHSST has had the 4th most Intel Science Talent Search Semifinalists (75) and the 6th most finalists (7) nationally.</li>
 
-    <li>The U.S. News and World Report has consistently ranked TJHSST in the top 5 high schools across the nation from 200 to 2013.</li>
+    <li>The U.S. News and World Report has consistently ranked TJHSST in the top 5 high schools across the nation from 2002 to 2013.</li>
 
     <li>TJHSST has the highest performances on the AP Calculus BC, AP Chemistry AP French Language, AP Government and Politics, AP U.S. History Exams.</li>
 
@@ -19,6 +23,11 @@
 <h4>Notable Vistiors</h4>
 
 <p>From 1986 to 2014, Thomas Jefferson has had countless key figures in all fields of study and research visit to speak and interact with the student body. Below are some of the key speakers that have visited:</p>
+
+<figure float="right" style="width: 200px">
+    <img src="/tsa/images/school/about/obama.jpg" width="200px" />
+    <figcaption>President Obama signs a patent bill into law.</figcaption>
+</figure>
 
 <h5>Politicians</h5>
 
