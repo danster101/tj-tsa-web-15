0c7bff988da9ffb53bc576ca82a144f35cb09829
Danial Hussain <dhussain101@gmail.com>
2015-04-30 13:37:41 -0400

    Won't you come on over baby

--- tsa/about.php
+++ tsa/about.php
@@ -21,3 +21,13 @@
     <p>I will strive to develop a cooperative attitude and will exercise tact and respect for other individuals. Through the work of my hands and mind, I will express my ideas to the best of my ability.</p>
     <p>I will make it my goal to do better each day the task before me, and to be steadfast in my belief in my God, and my fellow Americans."</p>
 </i>
+
+<h3>History</h3>
+
+<p>In 1978, the American Industrial Arts Student Association (AIASA) separated from the American Industrial Arts Association, becoming financially independent and recognized as the official vocational student organization for industrial arts students. One year later, AIASA held its first national conference, separate from the AIAA.</p>
+
+<p>In 1988, students voted to change AIASA's name to Technology Student Association (TSA), reflecting an expansion of focus from industrial arts to mainstream technology. The official logo was adopted in 1989. </p>
+
+<p>Since then, TSA has drastically expanded, gaining membership, establishing new chapters all across the country. TSA has formed partnerships with such organizations as the American Cancer Society, the International Technology and Engineering Educators Association, Project Lead The Way, and VEX Robotics, and, through those partnerships, has launched STEM initiatives, including TEAMS (Tests of Engineering Aptitude, Mathematics and Science) and the Verizon Innovative App Challenge, in addition to the wide range of competitive events, showcased at annual conferences at local, state, and national levels.</p>
+
+<p>Last year, 2013, marked TSA's 35th anniversary, and framed the largest national conference to date in Orlando, Florida. This year's national conference, to be hosted in Washington, D.C., promises to be even greater.</p>
--- tsa/index.php
+++ tsa/index.php
@@ -13,7 +13,6 @@
 						<div class="nav-indicator"></div>
 						<ul>
 							<li load="about" class="active"><span class="fa nav-icon fa-info-circle"></span>About</li>
-							<li load="history"><span class="fa nav-icon fa-history"></span>History</li>
 							<li load="events"><span class="fa nav-icon fa-briefcase"></span>Events</li>
 						</ul>
 					</nav>
@@ -21,10 +20,6 @@
 
 				<div class="content" id="about">
 					<?php include("about.php"); ?>
-				</div>
-
-				<div class="content hidden" id="history">
-					<?php include("history.php"); ?>
 				</div>
 
 				<div class="content hidden" id="events">
