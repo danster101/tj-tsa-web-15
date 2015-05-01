d0add044383b6caa25659a5ecc3b10cad3a63b12
bluepichu <github@bluepichu.com>
2015-05-01 03:27:24 -0400

    Shake it off, off, off

--- sources.php
+++ sources.php
@@ -7,7 +7,80 @@
 			<?php make_sidebar(); ?>
 			<main>
 				<article>
-					<p>Sources go here.</p>
+					<div class="content">
+
+						<h3>Home Page Slider<a name="index"></a></h3>
+						<ul class="arrow-bullet">
+							<li>TSA logo - <a target="_blank" href="http://tsaweb.org">National TSA Site</a></li>
+							<li>TJHSST logo - <a target="_blank" href="http://tjhsst.edu">TJHSST Site</a></li>
+							<li>TJHSST Sketch - <a target="_blank" href="http://tjhsst.edu">TJHSST Site</a></li>
+						</ul>
+
+						<h3>The Chapter</h3>
+
+						<h4>About<a name="chapter-about"></a></h4>
+
+						<ul class="arrow-bullet">
+							<li>All information - TJHSST TSA Webmaster Team</li>
+							<li>National TSA Conference 2014 Logo - <a target="_blank" href="http://www.prweb.com/releases/2013/7/prweb10929875.htm">PRWeb Article on 2014 National TSA Conference</a></li>
+							<li>Officers Picture - Various TJHSST TSA Photographers</li>
+						</ul>
+						
+						<h3>The School</h3>
+
+						<h4>About</h4>
+
+						<ul class="arrow-bullet">
+							<li>All information - <a target="_blank" href="http://en.wikipedia.org/wiki/Thomas_Jefferson_High_School_for_Science_and_Technology">Wikipedia on TJHSST</a></li>
+							<li>TJHSST Blueprint Image - <a target="_blank" href="http://bjuarchitects.com/test%20k-12%20education.html">K-12 Educational Architects Projects</a></li>
+							<li>Obama Signing <i>America Invents Act</i> - <a target="_blank" href="http://www.tjhsst.edu/parents-community/ptsa/documents/techcetera-2011-2012/Techcetera2011_October.pdf">TJHSST PTSA via Tech Cetera</a></li>
+						</ul>
+
+						<h4>STEM Curriculum<a name="tjhsst-curriculum"></a></h4>
+
+						<ul class="arrow-bullet">
+							<li>Course Descriptions - <a target="_blank" href="http://tjhsst.edu">TJHSST Site</a> (including the <a target="_blank" href="https://fcps.tjhsst.edu/coursemgmt/courses/300/">Course Guide</a>)</li>
+							<li>STEM Logo - <a target="_blank" href="http://www.apsu.edu/stem">Austin Peay State University</a></li>
+							<li>IBET Robotics Photo - <a target="_blank" href="http://www.tjtoday.org/4681/news/freshman-prepare-for-annual-ibet-robot-competition/#prettyPhoto/0/">tjTODAY Online</a></li>
+							<li>Other Photos - Taken by TJHSST TSA Webmaster Team</li>
+							<li>tjSTAR Logo - <a target="_blank" href="http://www.tjhsst.edu/students/activities/tjstar/">tjSTAR Site</a></li>
+						</ul>
+
+						<h4>STEM Extracurriculars<a name="tjhsst-extracurriculars"></a></h4>
+
+						<ul class="arrow-bullet">
+							<li>List of Activities - <a target="_blank" href="http://www.tjhsst.edu/students/activities/">TJHSST Activites Site</a></li>
+							<li>TJHSST Activities Word Map - <a target="_blank" href="http://www.tjhsst.edu/students/activities/">TJHSST Activites Site</a></li>
+							<li>Stembassadors Photo - <a target="_blank" href="http://www.tjhsst.edu/~emglazer/stembassadors/">TJHSST Stembassadors Site</a></li>
+							<li>Electronics Project Photo - <a target="_blank" href="https://www.tjhsst.edu/">TJHSST Site</a></li>
+							<li>HackTJ Photo - <a target="_blank" href="http://www.connectionnewspapers.com/news/2013/apr/30/programming-marathon-tj/">The Connection Newspaper</a></li>
+							<li>Chemistry Experiment Photo - <a target="_blank" href="http://www.tjhsst.edu/research-academics/research-labs/">TJHSST Research Labs Site</a></li>
+						</ul>
+
+						<h3>The Organization</h3>
+
+						<h4>About</h4>
+
+						<ul class="arrow-bullet">
+							<li>All information - <a target="_blank" href="http://tsaweb.org">National TSA Site</a></li>
+							<li>TSA logo - <a target="_blank" href="http://tsaweb.org">National TSA Site</a></li>
+							<li>AIASA logo - <a target="_blank" href="http://patsa.org/about">Pennsylvania TSA Site</a></li>
+						</ul>
+
+						<h4>Conferences</h4>
+
+						<ul class="arrow-bullet">
+							<li>All information - <a target="_blank" href="http://tsaweb.org">National TSA Site</a></li>
+						</ul>
+
+						<h4>Events</h4>
+
+						<ul class="arrow-bullet">
+							<li>List of Events - <a target="_blank" href="http://tsaweb.org">National TSA Site</a></li>
+							<li>All images - Various TJHSST TSA photographers</li>
+						</ul>
+
+					</div>
 				</article>
 			</main>
 		</div>
