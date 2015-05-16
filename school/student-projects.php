451ad2d919774b25414fd335a58e263261587ddd
Danial Hussain <dhussain101@gmail.com>
2015-05-16 15:59:07 -0400

    Hi

--- school/student-projects.php
+++ school/student-projects.php
@@ -4,16 +4,11 @@
 
     <li><b>Arbitrary-Constellation Quadrature Amplitude Modulation</b> by Matthew Savage (Microelectronics) <a href="http://seniortech.bluepichu.com" target="_blank">[link]</a> - This project builds off of the standard encoding method used by Wifi, Bluetooth, and 4G to function with arbitrary data mappings, potentially allowing for optimizations beyond the current industry standard.</li>
 
-    <li><b>Developing an Autonomous Underwater Microglider Prototype</b> by Danial Hussain (Oceanography) - Autonomous underwater gliders represent a new technology with the potential to revolutionize the methods with which scientists collect ocean data. Underwater gliders have a large cost-reduction potential compared to traditional methods of ocean sampling such as using research vessels which can cost thousands of dollars to operate for a single day. The hybrid microglider utilizes two buoyancy engines to change pitch and control roll.  The glider is equipped with a camera, and sonar sensor capable of mapping the seafloor.  The microglider also is equipped with a motor, so the craft can escape from strong currents or maintain a level depth while using the sonar sensor.</li>
-
-    <figure float="right" style="width: 250px">
-        <img src="/tsa/images/school/motionsensor.jpg" width="250px" />
-        <figcaption>Testing a Motion Sensor for use on the Glider.</figcaption>
-    </figure>
-    
-    <figure float="right" style="width: 250px">
-        <img src="/tsa/images/school/flowsim.jpg" width="250px" />
+    <figure float="right" style="width: 200px">
+        <img src="/tsa/images/school/flowsim.jpg" width="200px" />
         <figcaption>Learning to use fluid simulation with SolidWorks.</figcaption>
     </figure>
+    
+    <li><b>Developing an Autonomous Underwater Microglider Prototype</b> by Danial Hussain (Oceanography) - Autonomous underwater gliders represent a new technology with the potential to revolutionize the methods with which scientists collect ocean data. Underwater gliders have a large cost-reduction potential compared to traditional methods of ocean sampling such as using research vessels which can cost thousands of dollars to operate for a single day. The hybrid microglider utilizes two buoyancy engines to change pitch and control roll.  The glider is equipped with a camera, and sonar sensor capable of mapping the seafloor.  The microglider also is equipped with a motor, so the craft can escape from strong currents or maintain a level depth while using the sonar sensor.</li>
     
 </ul>
